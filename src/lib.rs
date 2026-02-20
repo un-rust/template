5bf71c1077e7db330348dc88bd8ada6a2e0fd570
hyq <betterhyq@qq.com>
2026-02-20 13:57:34 +0800

    test: add unit test for hello function in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,12 @@ pub fn hello(name: &str) -> String {
     log!(LogLevel::Info, "lib.rs");
     format!("Hello, {}!", name)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_hello() {
+        assert_eq!(hello("world"), "Hello, world!");
+    }
+}
